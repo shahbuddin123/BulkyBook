1ce2107c521bbd214a3a2dec4d6f19a0ea724745
Shahbuddin.d <Shahbuddin.d@HMECL003553>
2021-05-17 10:48:00 +0530

    Creating and configuring view start and imports

--- BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,10 +8,12 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BulkyBook.Controllers
+namespace BulkyBook.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
